16876a82c9bab3a0195776fe99dd96c5e4bf3c26
ian douglas <ian.douglas@iandouglas.com>
2016-10-25 00:51:38 -0600

    switched to mysql, added code to handle registration, login, and fetching users

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"time"
 	"database/sql"
-	"strings"
 	"bytes"
 
 	"github.com/GetStream/stream-go"
@@ -16,52 +15,80 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pborman/uuid"
+	"gopkg.in/gorp.v1"
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/aws/session"
+	//"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/jinzhu/gorm"
+	"strings"
 )
 
-type AccessToken struct {
-	Token  string
-	Expiry time.Time
-}
-
 type User struct {
-	ID       int64
-	UUID     string
-	Email    string
-	Username string
+	gorm.Model
+	UUID      string `gorm:"column:uuid" json:"uuid"`
+	Username  string `gorm:"column:username" json:"username"`
+	Email     string `gorm:"column:email" json:"email"`
 }
 
-type UserToken struct {
-	Token  string
-	Expiry time.Time
+type Photo struct {
+	gorm.Model
+	UserID    int  `gorm:"column:user_id,index" json:"user_id"`
+	UUID      string `gorm:"column:uuid" json:"uuid"`
+	URL       string `gorm:"column:url" json:"url"`
+	Likes     int `gorm:"column:likes" json:"likes"`
 }
 
 var router *gin.Engine
-var DB *sql.DB
 var S3Client *s3.S3
 var S3BucketName string = "getstream-example"
 
+type FeedItem struct {
+	ID          string `json:"id"`
+	AuthorEmail string `json:"author_email"`
+	AuthorName  string `json:"author_name"`
+	AuthorID    string `json:"author_id"`
+	PhotoURL    string `json:"photo_url"`
+	CreatedDate string `json:"created_date"`
+}
+
+var test_global_feed = []FeedItem{
+	{ID:"3", CreatedDate:"2016-10-19", AuthorID: uuid.New(), AuthorName:"thierry", AuthorEmail:"no_gravatar@example.com", PhotoURL:"http://i1054.photobucket.com/albums/s499/vadimzbanok/1327.jpg"},
+	{ID:"1", CreatedDate:"2016-10-17", AuthorID: uuid.New(), AuthorName:"ian", AuthorEmail:"ian.douglas@iandouglas.com", PhotoURL:"http://greenhackathon.com/graphics/greenhackathon_logo.png"},
+	{ID:"2", CreatedDate:"2016-10-18", AuthorID: uuid.New(), AuthorName:"josh", AuthorEmail:"joshtilton@gmail.com", PhotoURL:"http://clubpenguin.wikia.com/wiki/File:Funny_RP.PNG"},
+}
+var test_user_feed = []FeedItem{
+	{ID:"3", CreatedDate:"2016-10-19", AuthorID: uuid.New(), AuthorName:"thierry", AuthorEmail:"no_gravatar@example.com", PhotoURL:"https://pixabay.com/static/uploads/photo/2016/03/28/12/35/cat-1285634_1280.png"},
+	{ID:"2", CreatedDate:"2016-10-18", AuthorID: uuid.New(), AuthorName:"josh", AuthorEmail:"joshtilton@gmail.com", PhotoURL:"https://pixabay.com/static/uploads/photo/2015/04/14/08/52/lion-721836_1280.jpg"},
+}
+
+var dbmap = initDb()
+
+func initDb() *gorp.DbMap {
+	db, err := sql.Open("mysql", "stream:B4ck3nd!@/stream_backend?parseTime=true")
+	if err != nil || db == nil {
+		panic("failed to connect database: " + err.Error())
+	}
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	dbmap.AddTableWithName(User{}, "users").SetKeys(true, "ID")
+	dbmap.AddTableWithName(Photo{}, "photos").SetKeys(true, "ID")
+	err = dbmap.CreateTablesIfNotExists()
+	if err != nil {
+		panic("failed to create tables: " + err.Error())
+	}
+	return dbmap
+}
+
+var StreamClient = initStream()
 // Stream.io variables
-var client *getstream.Client
 var globalFeed *getstream.FlatFeed
 
-func main() {
-
-	// database setup
-	_, err := sql.Open("sqlite3", "./getstream-mobile-backend.db")
-	if err != nil {
-		fmt.Println("failed to connect database")
-	}
-	//defer DB.Close()
-
+func initStream() *getstream.Client {
 	// GetStream.io setup
-
-	client, err = getstream.New(&getstream.Config{
+	client, err := getstream.New(&getstream.Config{
 		APIKey:      os.Getenv("STREAM_API_KEY"),
 		APISecret:   os.Getenv("STREAM_API_SECRET"),
 		AppID:       os.Getenv("STREAM_APP_ID"),
@@ -74,22 +101,25 @@ func main() {
 	if err != nil {
 		panic("could not set global feed")
 	}
+	return client
+}
 
+func main() {
 	// S3
 	//Endpoint:         "s3.amazonaws.com"
 	//S3ForcePathStyle: true
-	S3Client = s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-	_, err = S3Client.CreateBucket(&s3.CreateBucketInput{
-		Bucket: &S3BucketName,
-	})
-	if err != nil {
-		log.Println("Failed to create bucket", err)
-		return
-	}
-	if err = S3Client.WaitUntilBucketExists(&s3.HeadBucketInput{Bucket: &S3BucketName}); err != nil {
-		log.Printf("Failed to wait for bucket to exist %s, %s\n", S3BucketName, err)
-		return
-	}
+	//S3Client = s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	//_, err = S3Client.CreateBucket(&s3.CreateBucketInput{
+	//	Bucket: &S3BucketName,
+	//})
+	//if err != nil {
+	//	log.Println("Failed to create bucket", err)
+	//	return
+	//}
+	//if err = S3Client.WaitUntilBucketExists(&s3.HeadBucketInput{Bucket: &S3BucketName}); err != nil {
+	//	log.Printf("Failed to wait for bucket to exist %s, %s\n", S3BucketName, err)
+	//	return
+	//}
 
 	// gin routing
 
@@ -112,13 +142,21 @@ func main() {
 	router.Static("/privacy", "privacy.html")
 	router.Static("/termsofservice", "termsofservice.html")
 
+	router.GET("/users", getUsers)
 	router.GET("/feed/:uuid", getFeed)
 	router.GET("/follow/:target", getFollow)
 	router.GET("/unfollow/:target", getUnfollow)
+	//router.POST("/like/:uuid", postLikePhoto)
+	//router.POST("/unlike/:uuid", postUnlikePhoto)
 
-	router.GET("/login", getLogin)
+	router.GET("/testfeed", func(c *gin.Context) {
+		// redirect to the repo, blog post, etc.
+		//c.Redirect(http.StatusTemporaryRedirect, "//getstream.io/blog")
+		c.JSON(http.StatusOK, gin.H{
+			"feed": test_global_feed,
+		})
+	})
 	router.POST("/login", postLogin)
-	router.GET("/register", getRegister)
 	router.POST("/register", postRegister)
 	router.POST("/upload", postPhotoUpload)
 
@@ -132,15 +170,18 @@ func main() {
 		})
 	})
 	log.Print("Listening on port 8080")
-	router.Run(":3000")
+	router.Run("0.0.0.0:3000")
 }
 
-func getFeed (c *gin.Context) {
-	userUUID := c.Param("userUUID")
-	lastUUID := c.Param("lastUUID")
+func getFeed(c *gin.Context) {
+	userUUID := c.Param("uuid")
+	lastUUID := c.Query("lastUUID")
+
+	log.Println("fetching feed for user", userUUID)
 	// get user stream, send back in json format
-	userFeed, err := client.FlatFeed("user", userUUID)
+	userFeed, err := StreamClient.FlatFeed("user", userUUID)
 	if err != nil {
+		log.Println("fetch feed threw an error")
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
@@ -151,6 +192,8 @@ func getFeed (c *gin.Context) {
 		options.IDGT = lastUUID
 	}
 	activities, err := userFeed.Activities(&options)
+
+	log.Println("returning activities")
 
 	c.JSON(http.StatusOK, gin.H{
 		"uuid": userUUID,
@@ -171,12 +214,12 @@ func getFollow(c *gin.Context) {
 		return
 	}
 
-	userFeed, err := client.FlatFeed("user", userUUID)
+	userFeed, err := StreamClient.FlatFeed("user", userUUID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 
-	targetFeed, err := client.FlatFeed("user", followUUID)
+	targetFeed, err := StreamClient.FlatFeed("user", followUUID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
@@ -203,12 +246,12 @@ func getUnfollow(c *gin.Context) {
 		return
 	}
 
-	userFeed, err := client.FlatFeed("user", userUUID)
+	userFeed, err := StreamClient.FlatFeed("user", userUUID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 
-	targetFeed, err := client.FlatFeed("user", unfollowUUID)
+	targetFeed, err := StreamClient.FlatFeed("user", unfollowUUID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
@@ -223,22 +266,56 @@ func getUnfollow(c *gin.Context) {
 }
 
 func postPhotoUpload(c *gin.Context) {
+	// handle upload in the background
+	userUUID := c.PostForm("uuid")
+	var user_id int = 0
+	err := dbmap.SelectOne(&user_id, "SELECT id FROM users WHERE uuid=?", userUUID)
+	if err != nil && err.Error() != "sql: no rows in result set" {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	if user_id == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "uuid not valid, photo not processed"})
+		return
+	}
+
+	_, header, err := c.Request.FormFile("upload")
+	log.Println(header.Filename)
+	localFilename := "./tmp/" + uuid.New() + ".png"
+	localSavedFile, err := os.Create(localFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	localSavedFile.Close()
+
+	var photo Photo
+	photo.UUID = uuid.New()
+	photo.UserID = user_id
+
+	insert, err := dbmap.Exec(`
+		INSERT INTO photos (uuid, user_id, CreatedAt, UpdatedAt)
+		VALUES (?, ?, ?, ?)`,
+		photo.UUID, user_id, time.Now(), time.Now())
+	if err != nil {
+		log.Println("sending error after photo insert")
+		c.JSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
+		return
+	}
+	_, err = insert.LastInsertId()
+	if err == nil {
+		log.Println("sending user payload response")
+		c.JSON(http.StatusCreated, gin.H{"uuid": photo.UUID, "status": "processing"})
+	} else {
+		log.Println("sending error response from insert")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
 	// create copy to be used inside the goroutine
 	cCp := c.Copy()
 	go func() {
+		log.Println("doing upload etc in the background")
 		var photoFilename string
-		var photoURL string
-
-		// handle upload in the background
-		userUUID := cCp.PostForm("uuid")
-		_, header, err := c.Request.FormFile("upload")
-		log.Println(header.Filename)
-		localFilename := "./tmp/" + uuid.New() + ".png"
-		localSavedFile, err := os.Create(localFilename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		localSavedFile.Close()
 
 		// shrink image
 		inImage, err := imaging.Open(localFilename)
@@ -249,11 +326,13 @@ func postPhotoUpload(c *gin.Context) {
 		imaging.Save(dstImage, photoFilename)
 
 		// push to S3, get URL
+		log.Println("push to S3")
 		file, err := os.Open(localFilename)
 		if err != nil {
 			panic(err)
 		}
 		defer file.Close()
+
 		fileInfo, _ := file.Stat()
 		var size int64 = fileInfo.Size()
 		buffer := make([]byte, size)
@@ -263,7 +342,7 @@ func postPhotoUpload(c *gin.Context) {
 		path := "photos/" + file.Name()
 		params := &s3.PutObjectInput{
 			Bucket:        aws.String(S3BucketName), // required
-			Key:           aws.String(path),       // required
+			Key:           aws.String(path), // required
 			ACL:           aws.String("public-read"),
 			Body:          fileBytes,
 			ContentLength: &size,
@@ -288,25 +367,25 @@ func postPhotoUpload(c *gin.Context) {
 			}
 		}
 		fmt.Println(awsutil.StringValue(result))
-		photoURL = "https://dvqg2dogggmn6.cloudfront.net/images/stream_logo.svg"
+		photo.URL = "http://unknown.image"
 
 		// send image url, date, username to stream
 
 		now := time.Now()
-		userFeed, err := client.FlatFeed("user", userUUID)
+		userFeed, err := StreamClient.FlatFeed("user", userUUID)
 		if err != nil {
 			fmt.Println(err)
 		} else {
 			_, err = globalFeed.AddActivity(&getstream.Activity{
 				Verb:      "photo",
-				ForeignID: uuid.New(),
+				ForeignID: photo.UUID,
 				TimeStamp: &now,
 				To:        []getstream.Feed{globalFeed, userFeed},
-				Object:    getstream.FeedID(fmt.Sprintf("photo:%s", photoFilename)),
+				Object:    getstream.FeedID(fmt.Sprintf("photo:%s", photo.UUID)),
 				Actor:     getstream.FeedID(fmt.Sprintf("user:%s", userUUID)),
 				MetaData:  map[string]string{
 					// add as many custom keys/values here as you like
-					"photoUrl": fmt.Sprintf("message %d", photoURL),
+					"photoUrl": photo.URL,
 				},
 			})
 			if err != nil {
@@ -316,20 +395,15 @@ func postPhotoUpload(c *gin.Context) {
 		// note that you are using the copied context "cCp", IMPORTANT
 		log.Println("Done! in path " + cCp.Request.URL.Path)
 	}()
-	c.JSON(http.StatusOK, gin.H{"status": "ok"})
-}
-
-func getRegister(c *gin.Context) {
-	c.HTML(http.StatusOK, "register.html", gin.H{})
 }
 
 func postRegister(c *gin.Context) {
-	var user *User
+	var user User
+	var output []string
 
 	email := c.PostForm("email")
 	username := c.PostForm("username")
-
-	var output []string
+	log.Println(email, username)
 
 	if username == "" || email == "" {
 		if username == "" {
@@ -339,71 +413,54 @@ func postRegister(c *gin.Context) {
 			output = append(output, "Email cannot be blank")
 		}
 	} else {
-		log.Println("checking username uniqueness")
-		rows, err := DB.Query(`
-			SELECT u.id, u.uuid. u.email, u.username FROM users u WHERE u.username=? OR u.email=?
-			LIMIT 1
-			`, strings.ToLower(username), strings.ToLower(email))
-		if err != nil {
-			log.Println("error getting user details for username", username)
+		var user_id int = 0
+		err := dbmap.SelectOne(&user_id, "SELECT id FROM users WHERE username=? or email=?",
+			strings.ToLower(username),
+			strings.ToLower(email))
+		if err != nil && err.Error() != "sql: no rows in result set" {
+			log.Println(err.Error())
 			c.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
-		for rows.Next() {
-			var userId int = -1
-			err = rows.Scan(&userId)
-			if userId > -1 {
-				output = append(output, "Username or Email already used")
-			}
+		if user_id > 0 {
+			output = append(output, "Username or Email already used")
 		}
 	}
 
 	if len(output) > 0 {
-		c.HTML(http.StatusOK, "register.html", gin.H{"errors": output})
+		log.Println("sending friendly errors")
+		c.JSON(http.StatusBadRequest, gin.H{"errors": output})
 		return
 	}
 
 	log.Println("saving new user in db")
-	// save user details in the db
-	stmt, err := DB.Prepare(`
-			INSERT INTO users (uuid, email, username, created_at, updated_at)
-			VALUES (?,?,?,?,?,?,?)`,
-	)
+	user.Username = username
+	user.Email = email
+	user.UUID = uuid.New()
+	insert, err := dbmap.Exec(`
+		INSERT INTO users (uuid, username, email, CreatedAt, UpdatedAt)
+		VALUES (?, ?, ?, ?, ?)`,
+		user.UUID, strings.ToLower(user.Username), strings.ToLower(user.Email), time.Now(), time.Now())
 	if err != nil {
-		panic(fmt.Sprintf("could create user insert statement, %v", err))
-	}
-	_, err = stmt.Exec(
-		uuid.New(),
-		user.Email,
-		user.Username,
-		time.Now(),
-		time.Now(),
-	)
-	if err != nil {
-		panic("failed to run user insert statement")
+		log.Println("sending error after insert")
+		c.JSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
+		return
 	}
 
-	//affect, err := res.RowsAffected()
-	//if err != nil {
-	//	panic("failed to run rows affected after user insert")
-	//}
-	//log.Println("rows affected:", affect)
-	//
-	//user.ID, err = res.LastInsertId()
-	//if err != nil {
-	//	panic("failed to get last_insert_id")
-	//}
-
-	c.JSON(http.StatusOK, gin.H{"status": "ok"})
-}
-
-func getLogin(c *gin.Context) {
-	c.HTML(http.StatusOK, "register.html", gin.H{})
+	id, err := insert.LastInsertId()
+	if err == nil {
+		log.Println("sending user payload response")
+		c.JSON(http.StatusCreated, gin.H{"uuid": user.UUID, "user_id": id})
+	} else {
+		log.Println("sending error response from insert")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	log.Println("ended up here, no response to send back")
 }
 
 func postLogin(c *gin.Context) {
+	var user User
 	var output []string
-	var userUUID string
 
 	email := c.PostForm("email")
 	username := c.PostForm("username")
@@ -416,27 +473,39 @@ func postLogin(c *gin.Context) {
 			output = append(output, "Email cannot be blank")
 		}
 	} else {
-		rows, err := DB.Query(`
-			SELECT u.UUID FROM users u WHERE u.username=? AND u.email=?
-			LIMIT 1
-			`, strings.ToLower(username), strings.ToLower(email))
-		if err != nil {
-			log.Println("error getting user details for username", username)
+		err := dbmap.SelectOne(&user, "SELECT users.* FROM users WHERE username=? and email=?",
+			strings.ToLower(username),
+			strings.ToLower(email))
+		if err != nil && err.Error() != "sql: no rows in result set" {
+			log.Println(err.Error())
 			c.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
-		for rows.Next() {
-			err = rows.Scan(
-				&userUUID,
-			)
-			if err != nil {
-				log.Println("error putting user details into struct")
-				c.JSON(http.StatusInternalServerError, err.Error())
-				return
-			}
+		if user.Username == "" {
+			output = append(output, "Username or Email not found, please register")
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"UUID": userUUID})
+	if len(output) > 0 {
+		log.Println("sending friendly errors")
+		c.JSON(http.StatusBadRequest, gin.H{"errors": output})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"UUID": user.UUID, "email": user.Email, "username": user.Username})
+	return
+}
+
+func getUsers(c *gin.Context) {
+	var users []User
+
+	_, err := dbmap.Select(&users, "SELECT * FROM users ORDER BY username")
+	if err != nil && err.Error() != "sql: no rows in result set" {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
 	return
 }
--- schema.sql
+++ schema.sql
@@ -1,20 +1,24 @@
--- sqlite3
-
-
-CREATE TABLE `users` (
-  `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
-  `uuid`          VARCHAR(36)  NULL,
-  `username`      VARCHAR(100) NULL,
-  `facebook_id`   VARCHAR(100) NULL,
-  `profile_image` VARCHAR(100) NULL,
-  `created_at`    DATE         NULL,
-  `updated_at`    DATE         NULL
+CREATE TABLE users
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  uuid VARCHAR(36),
+  username VARCHAR(100),
+  email VARCHAR(100),
+  created_at DATE,
+  updated_at DATE
 );
 
-CREATE TABLE `user_tokens` (
-  `uid`        INTEGER PRIMARY KEY,
-  `token`      VARCHAR(64) NULL,
-  `expiry`     DATE        NULL,
-  `created_at` DATE        NULL,
-  `updated_at` DATE        NULL
+
+CREATE TABLE photos
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  user_id INT(11),
+  uuid VARCHAR(36),
+  url VARCHAR(500),
+  likes INT(11) DEFAULT '0',
+  expiry DATE,
+  created_at DATE,
+  updated_at DATE,
+  CONSTRAINT photos_userid___fk FOREIGN KEY (user_id) REFERENCES users (id)
 );
+CREATE INDEX photos_userid___fk ON photos (user_id);
