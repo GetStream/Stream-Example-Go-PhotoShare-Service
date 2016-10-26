53779d1ad806f1699cc6e7a0283296f128880e5d
ian douglas <ian.douglas@iandouglas.com>
2016-10-26 11:58:56 -0600

    updated schema with fixtures

--- schema.sql
+++ schema.sql
@@ -1,27 +1,30 @@
 CREATE TABLE users
 (
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  uuid VARCHAR(36),
-  username VARCHAR(100),
-  email VARCHAR(100),
-  created_at DATE,
-  updated_at DATE
+  ID INT(10) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  CreatedAt DATETIME,
+  UpdatedAt DATETIME,
+  DeletedAt DATETIME,
+  UUID VARCHAR(255),
+  Username VARCHAR(255),
+  Email VARCHAR(255)
 );
+INSERT INTO users (ID, CreatedAt, UpdatedAt, DeletedAt, UUID, Username, Email) VALUES (1, '2016-10-25 06:11:51', '2016-10-25 06:11:51', null, '9cf34d34-a042-4231-babc-eee6ba67bd18', 'ian', 'ian@example.com');
+INSERT INTO users (ID, CreatedAt, UpdatedAt, DeletedAt, UUID, Username, Email) VALUES (2, '2016-10-26 17:33:35', '2016-10-26 17:33:35', null, '03a1cfed-3590-4aa8-a592-f78bc71ccfbd', 'josh', 'josh@getstream.io');
 
 
 CREATE TABLE photos
 (
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  user_id INT(11),
-  uuid VARCHAR(36),
-  url VARCHAR(500),
-  likes INT(11) DEFAULT '0',
-  expiry DATE,
-  created_at DATE,
-  updated_at DATE,
-  CONSTRAINT photos_userid___fk FOREIGN KEY (user_id) REFERENCES users (id)
+  ID INT(10) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  CreatedAt DATETIME,
+  UpdatedAt DATETIME,
+  DeletedAt DATETIME,
+  UserID INT(11),
+  UUID VARCHAR(255),
+  URL VARCHAR(255),
+  Likes INT(11)
 );
-CREATE INDEX photos_userid___fk ON photos (user_id);
+INSERT INTO photos (CreatedAt, UpdatedAt, DeletedAt, UserID, UUID, URL, Likes) VALUES ('2016-10-25 18:28:28', '2016-10-25 18:28:29', null, 1, '3c7c77bd-e1b4-4e64-9c9d-fff223efc17b', 'https://android-demo.s3.amazonaws.com/photos/f5222729-17d5-4b21-bade-a3e7ce1adb1c.png', null);
+
 
 CREATE TABLE follows
 (
@@ -29,3 +32,4 @@ CREATE TABLE follows
   user_id_1 INT(11),
   user_id_2 INT(11)
 );
+INSERT INTO follows (user_id_1, user_id_2) VALUES (1, 2);
